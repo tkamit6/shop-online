414220e2c2d90bf14130c5a591d31aaaddc47872
tkamit6 <76872254+tkamit6@users.noreply.github.com>
2024-01-25 19:59:41 +0530

    n

--- src/components/NextSession.jsx
+++ src/components/NextSession.jsx
@@ -4,13 +4,27 @@ import { Provider } from 'react-redux'
 import { persistor, store } from '@/redux/store'
 import { SessionProvider } from "next-auth/react"
 import { PersistGate } from 'redux-persist/integration/react'
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 export default function NextSession({ children }) {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#000', // Replace with your desired primary color
+            },
+        },
+        background: {
+            default: '#000', // Replace with your desired background color
+        },
+    });
+
     return <Provider store={store}>
         <PersistGate persistor={persistor}>
             <SessionProvider >
-                {children}
+                <ThemeProvider theme={theme}>
+                    {children}
+                </ThemeProvider>
             </SessionProvider>
         </PersistGate>
     </Provider>
--- src/components/SingleProducts.jsx
+++ src/components/SingleProducts.jsx
@@ -14,6 +14,8 @@ export default function SingleProducts({ product, idString }) {
     const dispatch = useDispatch();
     const { productData } = useSelector((state) => state?.shopping);
 
+
+    
     console.log(productData)
     useEffect(() => {
         const singleProduct = productData.filter((item) => {
@@ -49,7 +51,7 @@ export default function SingleProducts({ product, idString }) {
                 <div className='flex items-center gap-x-5'>
                     <button onClick={() => dispatch(addtoCart(product)) && toast.success(`${product?.title.substring(0, 10)} added to cart`)} className='bg-darkText hover:shadow-xl group text-slate-100 px-6 py-2 uppercase text-sm w-fit flex items-center gap-x-2 '>add to cart <IoMdCart className='group-hover:scale-125' /> </button>
                     {
-                        productQuantity > 0 && <div className='flex justify-center w-fit flex-1 items-center '> <Button className='w-[10%]' onClick={() => dispatch(decreaseQuantity(product))}>less</Button><p className='w-[10%] text-center font-bold'> {productQuantity} </p><Button className='w-[10%]' onClick={() => dispatch(increaseQuantity(product))}>more</Button> </div>
+                        productQuantity > 0 && <div className='flex justify-center w-fit flex-1 items-center '> <Button className='w-[20%]' onClick={() => dispatch(decreaseQuantity(product))}>less</Button><p className='w-[10%] text-center font-bold'> {productQuantity} </p><Button className='w-[20%]' onClick={() => dispatch(increaseQuantity(product))}>more</Button> </div>
                     }
                 </div>
 
